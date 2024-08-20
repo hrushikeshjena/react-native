5075f1501c7c05168d6beddfc57cde7cd5f02da9
hrushikeshjena <9hrushikesh@gmail.com>
2024-08-21 00:36:59 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hello, world!</Text>
+      <Button title="press here" />
+    </View>
+  );
+};
+
+export default App;
